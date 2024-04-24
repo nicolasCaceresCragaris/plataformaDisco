8875cce10868ddc6b44220c66526cd85f2b0cbb5
nicolasCaceresCragaris <caceres.nico24@gmail.com>
2024-04-24 02:59:18 -0300

    Estilo y disenio responsive agregado. Se agrego funcionalidad js que muestra un icon con nombre

--- tour/tour.js
+++ tour/tour.js
@@ -1,13 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-
+    function getTickets(){
+        swal("Sold!", "You have tickets to the " + place + 
+       " concert", "success");
+    }
     
     const bienvenida = document.querySelector("#welcome");
-    
+    const iconoTicket=document.querySelector("#iconoTicket");
+    const saludo= document.querySelector("#saludo");
+
+
     let nombre= prompt ("Ingrese su nombre");
+
+    while(nombre.length<3){
+
+        alert("Error nombre invalido");
+        nombre= prompt ("Ingrese su nombre");
+
+    };
     let edad= parseInt(prompt("Ingrese su edad"));
 
-  
+    iconoTicket.innerHTML='<i class="fa-solid fa-ticket"></i>';
+    saludo.textContent=`Hola,${nombre}`;
     bienvenida.textContent=`Bienvenido ${nombre} de ${edad} anios. Desea adquirir tickets`;
 
 });
+
+
+
+
+
