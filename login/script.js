4601cbc3134930b85c592b23d77a56604c2e453f
nicolasCaceresCragaris <caceres.nico24@gmail.com>
2024-05-03 21:09:45 -0300

    Libreria sweet alert, funcionalidad en botones

---
+++ login/script.js
@@ -0,0 +1,6 @@
+
+let email = document.getElementById('email').value;
+let password = document.getElementById('password').value;
+
+console.log("Email:"+email);
+console.log("Contra:"+password);
--- tour/tour.js
+++ tour/tour.js
@@ -1,32 +1,74 @@
-document.addEventListener("DOMContentLoaded", function() {
 
-    function getTickets(){
-        swal("Sold!", "You have tickets to the " + place + 
-       " concert", "success");
+const bienvenida = document.querySelector("#welcome");
+const iconoTicket=document.querySelector("#iconoTicket");
+const saludo= document.querySelector("#saludo");
+
+const entradas=[
+    {lugar:"carlos paz",cantidad:10},
+    {lugar:"villa fiorito",cantidad:10},
+    {lugar:"Bella vista",cantidad:0}
+]
+    
+
+
+//FUNCIONALIDAD DE NO PERMITIR SI ES UN NOMBRE CORTO
+
+let nombre= prompt ("Ingrese su nombre");
+
+while(nombre.length<3){
+
+    alert("Error nombre invalido");
+    nombre= prompt ("Ingrese su nombre");
+
+};
+
+//FUNCIONALIDAD DE PONER EL TICKET CON EL NOMBRE
+let edad= parseInt(prompt("Ingrese su edad"));
+
+iconoTicket.innerHTML='<i class="fa-solid fa-ticket"></i>';
+saludo.textContent=`Hola,${nombre}`;
+ bienvenida.textContent=`Bienvenido ${nombre} de ${edad} anios. Desea adquirir tickets`;
+
+
+//FUNCIONALIDAD DE SWEET ALERT
+
+const botonCompra= document.getElementsByClassName("botonCompra");
+
+
+function getTickets(entradas,i){
+
+    
+
+    if(entradas[i].cantidad>0){
+        
+        devolver=entradas[i];
+        entradas[i].cantidad--;
+
+        return true;
+    }
+    if(entradas[i].cantidad<=0){
+
+        return false;
     }
     
-    const bienvenida = document.querySelector("#welcome");
-    const iconoTicket=document.querySelector("#iconoTicket");
-    const saludo= document.querySelector("#saludo");
+}
 
 
-    let nombre= prompt ("Ingrese su nombre");
+for (let i = 0; i < botonCompra.length; i++) {
 
-    while(nombre.length<3){
+    if(edad>=18){
 
-        alert("Error nombre invalido");
-        nombre= prompt ("Ingrese su nombre");
+        botonCompra[i].addEventListener("click", function() {
+            if(getTickets(entradas,i)){
+                swal("Compra exitosa","Gracias","success");
+            }else{
+                botonCompra[i].textContent="Sold Out";
+                swal("No se pudo efectuar","Lo sentimos","error");
+            }
 
-    };
-    let edad= parseInt(prompt("Ingrese su edad"));
-
-    iconoTicket.innerHTML='<i class="fa-solid fa-ticket"></i>';
-    saludo.textContent=`Hola,${nombre}`;
-    bienvenida.textContent=`Bienvenido ${nombre} de ${edad} anios. Desea adquirir tickets`;
-
-});
-
-
-
-
+        });
+    }else{
+        swal("No podes comprar entradas","Sos menor","warning")
+    }
+}
 
