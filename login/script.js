2ab0ee44dfb1429ee3bf361c4b5fd53702055a84
nicolasCaceresCragaris <caceres.nico24@gmail.com>
2024-05-07 07:22:06 -0300

    Validacion de login agregada

--- login/script.js
+++
@@ -1,6 +0,0 @@
-
-let email = document.getElementById('email').value;
-let password = document.getElementById('password').value;
-
-console.log("Email:"+email);
-console.log("Contra:"+password);
---
+++ validation.js
@@ -0,0 +1,39 @@
+const selectForm = document.forms;
+
+let mensajeDeError="Error:";
+
+let errores=0;
+
+
+selectForm[0].addEventListener("submit",function(e){
+
+
+        for(let i=0;i<selectForm[0].elements.length-1;i++){
+
+            if(selectForm[0].elements[i].value.length==0){
+
+                mensajeDeError=mensajeDeError.concat(` Complete ${selectForm[0].elements[i].name}.`);
+
+                errores++;
+            }
+        }
+
+        if(errores==0){
+
+            e.preventDefault();
+            swal ( "Exitos" ,  "Recibimos su peticion" ,  "success" );
+
+        }
+
+
+        if(errores>0){
+
+            e.preventDefault();
+            swal ( "Error" ,  mensajeDeError ,  "error" );
+            mensajeDeError="Error:";   
+            errores=0;
+        }
+
+     
+})
+
